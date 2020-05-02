28101cbe52c16b1421deb82d345746bdd37449ec
sergBull <bullsergv@gmail.com>
2020-05-02 12:52:39 +0300

    Update Renderer.js

--- src/Renderer.js
+++ src/Renderer.js
@@ -1,6 +1,6 @@
 function createScreen(w, h) {
 	var canvas = document.createElement("canvas");
-	var ctx = ctx.getContext("2d");
+	var ctx = canvas.getContext("2d");
 	
 	document.body.appendChild(canvas);
 	
