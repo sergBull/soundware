4f94bc3968ebcf39147abd2e8405fafbd9a9ca57
sergBull <bullsergv@gmail.com>
2020-05-02 13:26:24 +0300

    Update Main.js

--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,5 @@
 var canvas = createScreen(window.innerWidth, window.innerHeight);
 var buffer = setScreenBuffer(0, 0, canvas.width, canvas.height);
 setPixel(buffer, 128, 128, 255, 0, 0, 127);
-point(buffer, [ 256, 256 ], [ 3, 4 ], [ 255, 127, 63, 127 ]);
+point(buffer, [ 256, 256 ], [ 3, 4 ], [ 255, 127, 255, 127 ]);
 render(buffer, canvas);
