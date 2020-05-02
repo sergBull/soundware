16cabb5296603d804a5d6dc51e8ea9826140b03a
sergBull <bullsergv@gmail.com>
2020-05-02 12:14:19 +0300

    Create Main.js

---
+++ src/Main.js
@@ -0,0 +1,5 @@
+var canvas = createScreen(window.innerWidth, window.innerHeight);
+
+var buffer = setScreenBuffer(canvas.width / 4, canvas.height / 4, canvas.width / 2, canvas.height / 2);
+
+render(buffer, canvas);
